3193b981e7986c5ae40dd813e800dd53b678b4dd
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-04-22 16:58:25 +0200

    Adding missing whitespace.

--- recompile_haml.sh
+++ recompile_haml.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
-while true; do
-  find . -name "*.haml" -type f -print | sed -e "p;s/\.haml$/\.html/" | xargs -n2 haml
-  find . -name "styles.sass" -type f -print | sed -e "p;s/\.sass$/\.css/" | xargs -n2 sass
-done
+find . -name "*.haml" -type f -print | sed -e "p;s/\.haml$/\.html/" | xargs -n2 haml
+find . -name "styles.sass" -type f -print | sed -e "p;s/\.sass$/\.css/" | xargs -n2 sass
